b361feb7b39d7cd18762b96ff4241145b26cd27b
mer1ch <mericburakacar@hotmail.com>
2025-07-29 11:15:51 +0300

    Tables and Migration

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        //int x;----> Field
+        //int y { get; set; }---->Property
+        //void test()
+        //{
+        //    int z; ----> Değişken(Variable)
+        //}
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
